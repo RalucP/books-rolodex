e8abbf0a1c7cde939b73574bf04e07b428f96da3
Raluca Parvan <parvanraluca0@gmail.com>
2023-10-24 20:09:40 +0200

    Add data and search bar feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Component } from 'react'
+import booksData from './data/books.json'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+class App extends Component {
+  constructor(){
+    super();
 
-  return (
-    <>
+    this.state = {
+      books: [],
+      searchField: ''
+    }
+  }
+
+  componentDidMount(){
+    this.setState({
+      books: booksData
+    })
+  }
+
+  onSearchChange = (event) => {
+    this.setState(() => {
+      const searchField = event.target.value.toLowerCase();
+      return { searchField }
+    });
+  }
+
+  render(){
+    
+    const { books, searchField } = this.state;
+    const { onSearchChange } = this;
+    
+    const filteredBooks = books.filter( book => book.name.toLowerCase().includes(searchField))
+
+    return (
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Books</h1>
+        <input type="search" className='serach-bar' placeholder='Search book' onChange={onSearchChange} />
+        {filteredBooks.map(book => (
+            <p key={book.book_id}>{book.name}</p>
+          ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    )
+  }
+
 }
 
 export default App
